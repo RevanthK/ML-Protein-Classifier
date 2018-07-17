ff05830519529e50c53c8da6878f012ed8d26c5c
Revanth <revkrock@gmail.com>
2018-07-16 23:24:06 -0700

    added python module

--- FeatureDetector.py
+++ FeatureDetector.py
@@ -3,55 +3,38 @@ from Bio.Seq import Seq
 #from Bio.Alphabet import IUPAC
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
 
-Pro_count=0
-Euk_count=0
-
-with open ("compiled.csv", "w") as compiled:
-	with open("Arch/Arch.csv", "w") as output:
-		output.write("Length,A,C,E,D,G,F,I,H,K,M,L,N,Q,P,S,R,T,W,V,Y,Class\n")
+def generateOuput(ArchaeaFile, ProkaryoteFile, EukaryoteFile):
+	Pro_count=0
+	Euk_count=0
+	with open ("output.csv", "w") as compiled:
 		compiled.write("Length,A,C,E,D,G,F,I,H,K,M,L,N,Q,P,S,R,T,W,V,Y,Class\n")
-		for seq_record in SeqIO.parse("Arch/ArchaeaData.fasta", "fasta"):
+		#for seq_record in SeqIO.parse("Arch/ArchaeaData.fasta", "fasta"):
+		for seq_record in SeqIO.parse(ArchaeaFile, "fasta"):	
 			analysed_seq = ProteinAnalysis(str(seq_record.seq))
-			output.write(str(len(seq_record)) + ",")
-			for amino_acid in analysed_seq.get_amino_acids_percent():
-				output.write(str(analysed_seq.get_amino_acids_percent()[amino_acid]) + ",")
-			output.write("P" + "\n")
-
 			compiled.write(str(len(seq_record)) + ",")
 			for amino_acid in analysed_seq.get_amino_acids_percent():
 				compiled.write(str(analysed_seq.get_amino_acids_percent()[amino_acid]) + ",")
 			compiled.write("P" + "\n")
 			Pro_count += 1
 
-	with open("Pro/Pro.csv", "w") as output:
-		output.write("Length,A,C,E,D,G,F,I,H,K,M,L,N,Q,P,S,R,T,W,V,Y,Class\n")
-		for seq_record in SeqIO.parse("Pro/ProkaryoteData.fasta", "fasta"):
+		#for seq_record in SeqIO.parse("Pro/ProkaryoteData.fasta", "fasta"):
+		for seq_record in SeqIO.parse(ProkaryoteFile, "fasta"):
 			analysed_seq = ProteinAnalysis(str(seq_record.seq))
-			output.write(str(len(seq_record)) + ",")
-			for amino_acid in analysed_seq.get_amino_acids_percent():
-				output.write(str(analysed_seq.get_amino_acids_percent()[amino_acid]) + ",")
-			output.write("P" + "\n")
-
 			compiled.write(str(len(seq_record)) + ",")
 			for amino_acid in analysed_seq.get_amino_acids_percent():
 				compiled.write(str(analysed_seq.get_amino_acids_percent()[amino_acid]) + ",")
 			compiled.write("P" + "\n")
 			Pro_count += 1
 
-	with open("euk/Euk.csv", "w") as output:
-		output.write("Length,A,C,E,D,G,F,I,H,K,M,L,N,Q,P,S,R,T,W,V,Y,Class\n")
-		for seq_record in SeqIO.parse("Euk/EukaryoteData.fasta", "fasta"):
+		#for seq_record in SeqIO.parse("Euk/EukaryoteData.fasta", "fasta"):
+		for seq_record in SeqIO.parse(EukaryoteFile, "fasta"):
 			analysed_seq = ProteinAnalysis(str(seq_record.seq))
-			output.write(str(len(seq_record)) + ",")
-			for amino_acid in analysed_seq.get_amino_acids_percent():
-				output.write(str(analysed_seq.get_amino_acids_percent()[amino_acid]) + ",")
-			output.write("E" + "\n")
-
 			compiled.write(str(len(seq_record)) + ",")
 			for amino_acid in analysed_seq.get_amino_acids_percent():
 				compiled.write(str(analysed_seq.get_amino_acids_percent()[amino_acid]) + ",")
 			compiled.write("E" + "\n")
 			Euk_count += 1
-print("Prokaryote count: " + str(Pro_count))
-print("Eukaryote count: " + str(Euk_count))
+
+	print("Prokaryote count: " + str(Pro_count))
+	print("Eukaryote count: " + str(Euk_count))
 
---
+++ test.py
@@ -0,0 +1,4 @@
+import FeatureDetector
+
+#takes 3 paramters. 3 strings that contain fasta file locations of archaea, prokaryotes, and eukaryotes
+FeatureDetector.generateOuput("Arch/ArchaeaData.fasta","Pro/ProkaryoteData.fasta","Euk/EukaryoteData.fasta");
